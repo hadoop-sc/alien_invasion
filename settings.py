4cfb40392cd9b014a53f158c1588bd375c3bd806
hadoop-sc <771729797@qq.com>
2019-09-11 17:57:36 +0800

    first commit

--- settings.py
+++ settings.py
@@ -13,4 +13,4 @@ class Settings(object):
         self.screen_caption = "Alien Invasion"
         self.screen_width = 1200
         self.screen_height = 800
-        self.screen_bg_color = (0, 0, 0)
+        self.screen_bg_color = (230, 230, 230)
